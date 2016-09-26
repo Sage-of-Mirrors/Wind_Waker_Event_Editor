6d6fde91af2435f7c8f051df006f86f637249162
Sage-of-Mirrors <empressglatia@gmail.com>
2016-09-25 21:08:33 -0500

    Added class skeletons and the node graph library

---
+++ Wind_Waker_Event_Editor/src/Event_ListLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wind_Waker_Event_Editor.src
+{
+    class Event_ListLoader
+    {
+    }
+}
